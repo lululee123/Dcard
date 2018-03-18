f307464c1978e386a0b11f9fd21bba27490e24fc
LuluLee <lulululu123456123456@gmail.com>
2018-03-19 01:53:00 +0800

    add firebase

--- function.js
+++ function.js
@@ -3,27 +3,64 @@ function newElement() {
   if (inputValue === '') {
     alert("任務不能為空！！");
   } else {
-    $("#myUL").append('<li><span>' + $("#myInput").val() + '</span> <small><a href="#edit">Edit</a>  <a href="#delete">Delete</a></small></li>');
-    // clear the text
-  }
+    var addClass =  $('li').size();
+    addClass = addClass + 1;
+    $("#myUL").append('<li class='+ addClass +' ><span>' + $("#myInput").val() + '</span> <small><a href="#edit">EDIT</a>  <a href="#delete">DELETE</a></small></li>');
+    firebase.auth().onAuthStateChanged(function(user) {
+    var num =  $("li").size();
+    var dataObj = firebase.database().ref().child(user.uid).child(num).update(
+      {
+        "inputValue":inputValue,
+        "checked": "no"
+      })
+    })
+  } 
+  // clear the text
   $("#myInput").val("");
 };
 
 // on clicking the add button
 $("#add").click(newElement);
-// delegate the events to dynamically generated elements
+
 // for the edit button
 $(document).on("click", 'a[href="#edit"]', function () {
   // make the span editable and focus it
   $(this).closest("li").find("span").prop("contenteditable", true).focus();
+  $(this).closest("li").find("small").append('<a href="#doneedit">DONE</a>');
   return false;
 });
+
+if ($(this).closest("li").find("span").focus()){
+  this.disabled = true;
+}
 // for the delete button
 $(document).on("click", 'a[href="#delete"]', function () {
   // remove the list item
-  $(this).closest("li").fadeOut(function () {
-    $(this).remove();
-  });
+    $(this).closest("li").fadeOut(function () { 
+      var num = $(this).attr('class'); 
+      firebase.auth().onAuthStateChanged(function(user) {
+       var dataObj = firebase.database().ref().child(user.uid).child(num).remove();
+      })
+     $(this).remove();
+    });
+  return false;
+});
+
+//doneedit button
+$(document).on("click", 'a[href="#doneedit"]', function () {
+  var num = $(this).closest("li").attr('class'); 
+  if (num.indexOf('checked') >= 0){
+    num = num.replace(/ checked/, "");
+  }
+  console.log(num);
+  var task = $(this).closest("li")[0].children["0"].innerText;
+  firebase.auth().onAuthStateChanged(function(user) {
+    var dataObj = firebase.database().ref().child(user.uid).child(num).update(
+      {
+        "inputValue": task
+      })
+  })
+  $('a[href="#doneedit"]').remove();
   return false;
 });
 
@@ -39,4 +76,40 @@ function notDoneFilter() {
 
 function noFilter() {
   $("li").css('display','block');
+}
+
+//don't show logout if not login
+$(".logout").css('display','none');
+
+//check login or signup
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+
+    $(".login").css('display','none');
+    $(".signup").css('display','none');
+    $(".logout").css('display','block');
+
+    var todoTask = firebase.database().ref().child(user.uid);
+    todoTask.on("child_added", snap => {
+      var status = snap.child("checked").val();
+      var detail = snap.child("inputValue").val();
+      $("#myUL").append('<li class=' + snap.key + '><span>' + detail + '</span> <small><a href="#edit">EDIT</a>  <a href="#delete">DELETE</a></small></li>');
+      if (status == "yes"){
+        var a = snap.key;
+        document.getElementsByClassName(a)[0].classList.toggle('checked');
+      }
+    })
+  } else {
+    // No user is signed in.
+    $(".login").css('display','block');
+    $(".signup").css('display','block');
+    $(".logout").css('display','none');
+
+  }
+});
+
+function logout(){
+  firebase.auth().signOut();
+  location.reload();
 }
